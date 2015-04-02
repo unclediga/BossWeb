7d1675b9d4091c568d1062deb3a32d4d62479557
BorodinovI <igordin@yandex.ru>
2015-04-02 20:07:06 +0300

    Добавил Tomcat в Application Servers Бился с запуском на Tomcat

---
+++ back-end/src/org/jee/TestServlet.java
@@ -0,0 +1,16 @@
+package org.jee;
+
+import java.io.IOException;
+
+/**
+ *
+ */
+public class TestServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
