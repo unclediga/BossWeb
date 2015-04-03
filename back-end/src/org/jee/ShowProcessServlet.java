0fe111cb4c3af368581bf9d04843642972c09b24
BorodinovI <igordin@yandex.ru>
2015-04-03 18:07:58 +0300

    back-end сделал. Запустил на Tomcat.

---
+++ back-end/src/org/jee/ShowProcessServlet.java
@@ -0,0 +1,40 @@
+package org.jee;
+
+import winutils.WinService;
+import winutils.WinUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ *
+ */
+public class ShowProcessServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h2>BEGIN Processes:</h2>");
+        out.println("<ul>");
+
+        WinService service =  WinUtils.getService();
+        String[] procs = service.getProcessList("chrome.exe");
+        for (int i = 0; i < procs.length; i++) {
+            out.println("<li>" + procs[i] + "</li>");
+        }
+
+        out.println("<ul>");
+        out.println("<h2>END Processes:</h2>");
+        out.println("</body></html>");
+        out.close();
+
+
+    }
+}
--- back-end/src/org/jee/TestServlet.java
+++ back-end/src/org/jee/TestServlet.java
@@ -1,16 +1,19 @@
 package org.jee;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  *
  */
 public class TestServlet extends javax.servlet.http.HttpServlet {
     protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        doGet(request, response);
     }
 
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+        PrintWriter out = response.getWriter();
+        out.println("<HTML><BODY>Hello from servlet!</HTML></BODY>");
+        out.close();
     }
 }
